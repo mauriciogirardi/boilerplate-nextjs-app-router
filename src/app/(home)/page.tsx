a3d4e446f704c2328c59ef09766266ecc36d091f
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-01-09 09:59:10 +0000

    keep calm and commint

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center bg-black  text-white">
+      <h1>Boilerplate NextJs</h1>
+    </div>
+  )
+}
